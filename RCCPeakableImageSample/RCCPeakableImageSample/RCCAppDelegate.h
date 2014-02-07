9d4077826734b5c96973585a53995062d6f47179
Roberto Seidenberg <roberto.seidenberg@me.com>
2014-02-07 15:01:36 +0100

    Initial commit

---
+++ RCCPeakableImageSample/RCCPeakableImageSample/RCCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RCCAppDelegate.h
+//  RCCPeakableImageSample
+//
+//  Created by Roberto Seidenberg on 07.02.14.
+//  Copyright (c) 2014 RCCBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RCCPeakableImageSample/RCCPeakableImageSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RCCPeakableImageSample
+//
+//  Created by Roberto Seidenberg on 07.02.14.
+//  Copyright (c) 2014 RCCBox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RCCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([RCCAppDelegate class]));
+  }
+}
